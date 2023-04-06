d47f88a16a052f671b1e3c51685947a3bd72af8d
Anna <annaidzkowska_00@onet.pl>
2023-04-06 16:45:34 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Connections from "./scenes/connections";
+import Patients from "./scenes/patients";
+import Patients2 from "./scenes/patient";
+
+import Projects from "./scenes/projects";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [theme, colorMode] = useMode();
+	const [isSidebar, setIsSidebar] = useState(true);
+
+	return (
+		<ColorModeContext.Provider value={colorMode}>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+				<div className='app'>
+					<Sidebar isSidebar={isSidebar} />
+					<main className='content'>
+						<Topbar setIsSidebar={setIsSidebar} />
+						<Routes>
+							<Route
+								path='/'
+								element={<Dashboard />}
+							/>
+							<Route
+								path='/connections'
+								element={<Connections />}
+							/>
+							<Route
+								path='/patients'
+								element={<Patients />}
+							/>
+							
+							<Route
+								path='/patient'
+								element={<Patients2 />}
+							/>
+							<Route
+								path='/projects'
+								element={<Projects />}
+							/>
+							<Route
+								path='/form'
+								element={<Form />}
+							/>
+							<Route
+								path='/bar'
+								element={<Bar />}
+							/>
+							<Route
+								path='/line'
+								element={<Line />}
+							/>
+						</Routes>
+					</main>
+				</div>
+			</ThemeProvider>
+		</ColorModeContext.Provider>
+	);
 }
 
 export default App;
---
+++ src/scenes/patients/index.jsx
@@ -0,0 +1,165 @@
+import { Box, Button } from "@mui/material";
+import { DataGrid, GridToolbar, useGridApiRef } from "@mui/x-data-grid";
+import { tokens } from "../../theme";
+import { mockDataContacts } from "../../data/mockData";
+import Header from "../../components/Header";
+import { useTheme } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import EditIcon from "@mui/icons-material/EditOutlined";
+
+
+const Patients = () => {
+		const theme = useTheme();
+		const colors = tokens(theme.palette.mode);
+	const apiRef = useGridApiRef();
+
+	const deletePatient = (id) => {
+		const result = patients.filter((patient) => patient.id !== id);
+		setPatients(result);
+	};
+	const editPatient = (event, id) => {
+		apiRef.current.startRowEditMode({id})
+	};
+
+	const defaultColumns = [
+		{ field: "id", headerName: "ID", flex: 0.5 },
+		{ field: "registrarId", headerName: "Numer rejestracyjny", flex: 0.5 },
+		{
+			field: "name",
+			headerName: "Imię i Nazwisko",
+			flex: 1,
+			cellClassName: "name-column--cell",
+			editable: false,
+		},
+		{
+			field: "age",
+			headerName: "Wiek",
+			type: "number",
+			headerAlign: "left",
+			align: "left",
+			editable: true,
+		},
+		{
+			field: "phone",
+			headerName: "Numer telefonu",
+			flex: 1,
+			editable: true,
+		},
+		{
+			field: "email",
+			headerName: "Email",
+			flex: 1,
+			editable: true,
+		},
+		{
+			field: "address",
+			headerName: "Adres",
+			flex: 1,
+			editable: true,
+		},
+		{
+			field: "city",
+			headerName: "Miasto",
+			flex: 1,
+			editable: true,
+		},
+		{
+			field: "zipCode",
+			headerName: "Kod pocztowy",
+			flex: 0.5,
+			editable: true,
+		},
+		{
+			field: "icon",
+			headerName: "Actions",
+			flex: 1,
+			sortable: false,
+			filterable: false,
+			renderCell: (event, params) => (
+				<>
+					<Button>
+						<EditIcon
+							style={{ color: colors.grey[100] }}
+							sx={{
+								margin: "25px",
+							}}
+							onClick={() => {
+								editPatient(event, event.row.id);
+							}}
+						/>
+					</Button>
+					<Button>
+						<DeleteIcon
+							style={{ color: colors.grey[100] }}
+							sx={{
+								margin: "25px",
+							}}
+							onClick={() => {
+								deletePatient(event.row.id);
+							}}
+						/>
+					</Button>
+				</>
+			),
+		},
+	];
+
+	const [patients, setPatients] = useState(mockDataContacts);
+	const [columns, setColumns] = useState(defaultColumns);
+	const gridRef = React.useRef(null);
+	const getRowId = (row) => row.id;
+
+	return (
+		<Box m='20px'>
+			<Header
+				title='DANE ADRESOWE'
+				subtitle='Tabela z podstawowymi danymi pacjentów'
+			/>
+			<Box
+				m='40px 0 0 0'
+				height='75vh'
+				sx={{
+					"& .MuiDataGrid-root": {
+						border: "none",
+					},
+					"& .MuiDataGrid-cell": {
+						borderBottom: "none",
+					},
+					"& .name-column--cell": {
+						color: colors.greenAccent[300],
+					},
+					"& .MuiDataGrid-columnHeaders": {
+						backgroundColor: colors.blueAccent[700],
+						borderBottom: "none",
+					},
+					"& .MuiDataGrid-virtualScroller": {
+						backgroundColor: colors.primary[400],
+					},
+					"& .MuiDataGrid-footerContainer": {
+						borderTop: "none",
+						backgroundColor: colors.blueAccent[700],
+					},
+					"& .MuiCheckbox-root": {
+						color: `${colors.greenAccent[200]} !important`,
+					},
+					"& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+						color: `${colors.grey[100]} !important`,
+					},
+					
+				}}>
+				<DataGrid
+					rows={patients}
+					columns={defaultColumns}
+					components={{ Toolbar: GridToolbar }}
+					ref={gridRef}
+					editMode='row'
+					getRowId={getRowId}
+					apiRef={apiRef}
+				/>
+			</Box>
+		</Box>
+	);
+};
+
+export default Patients;
